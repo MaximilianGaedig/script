b484bac056bc2fa4eae75dbf9cc5cadf4100c838
anupritaisno1 <www.anuprita804@gmail.com>
2020-05-22 16:52:21 +0200

    remove deprecated --block argument
    Signed-off-by: anupritaisno1 <www.anuprita804@gmail.com>

--- generate_delta.sh
+++ generate_delta.sh
@@ -19,7 +19,7 @@ trap "rm -rf \"$KEY_DIR\"" EXIT
 cp "$PERSISTENT_KEY_DIR"/* "$KEY_DIR"
 script/decrypt_keys.sh "$KEY_DIR"
 
-./build/tools/releasetools/ota_from_target_files --block "${EXTRA_OTA[@]}" -k "$KEY_DIR/releasekey" \
+./build/tools/releasetools/ota_from_target_files "${EXTRA_OTA[@]}" -k "$KEY_DIR/releasekey" \
     -i releases/$OLD/release-$DEVICE-$OLD/$DEVICE-target_files-$OLD.zip \
     releases/$NEW/release-$DEVICE-$NEW/$DEVICE-target_files-$NEW.zip \
     releases/$NEW/$DEVICE-incremental-$OLD-$NEW.zip
