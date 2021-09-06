a27bbf5d8d1190e8f15ac810dd8b8ea33efcf9f5
MaximilianGaedig <38767445+MaximilianGaedig@users.noreply.github.com>
2021-09-06 21:04:34 +0200

    get password from keepass

--- decrypt_keys.sh
+++ decrypt_keys.sh
@@ -7,31 +7,31 @@ source "$(dirname ${BASH_SOURCE[0]})/common.sh"
 [[ $# -ne 1 ]] && user_error "expected 1 argument (key directory)"
 
 cd $1
-
-[[ "${password+defined}" = defined ]] || read -p "Enter key passphrase (empty if none): " -s password
+export password_avb=$(secret-tool lookup avb.pem avb.pem)
+#[[ "${password+defined}" = defined ]] || read -p "Enter key passphrase (empty if none): " -s password
 echo
 
 tmp="$(mktemp -d /dev/shm/decrypt_keys.XXXXXXXXXX)"
 trap "rm -rf \"$tmp\"" EXIT
 
-export password
+export password=""
 
 for key in releasekey platform shared media networkstack; do
-    if [[ -n $password ]]; then
-        openssl pkcs8 -inform DER -in $key.pk8 -passin env:password | openssl pkcs8 -topk8 -outform DER -out "$tmp/$key.pk8" -nocrypt
-    else
+#    if [[ -n $password ]]; then
+#        openssl pkcs8 -inform DER -in $key.pk8 -passin env:password | openssl pkcs8 -topk8 -outform DER -out "$tmp/$key.pk8" -nocrypt
+#    else
         openssl pkcs8 -topk8 -inform DER -in $key.pk8 -outform DER -out "$tmp/$key.pk8" -nocrypt
-    fi
+#    fi
 done
 
-if [[ -f avb.pem ]]; then
-    if [[ -n $password ]]; then
-        openssl pkcs8 -topk8 -in avb.pem -passin env:password -out "$tmp/avb.pem" -nocrypt
-    else
-        openssl pkcs8 -topk8 -in avb.pem -out "$tmp/avb.pem" -nocrypt
-    fi
-fi
+#if [[ -f avb.pem ]]; then
+#    if [[ -n $password ]]; then
+        openssl pkcs8 -topk8 -in avb.pem -passin env:password_avb -out "$tmp/avb.pem" -nocrypt
+#    else
+#        openssl pkcs8 -topk8 -in avb.pem -out "$tmp/avb.pem" -nocrypt
+#    fi
+#fi
 
 unset password
-
+unset password_avb
 mv "$tmp"/* .
