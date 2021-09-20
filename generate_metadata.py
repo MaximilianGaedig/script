b22af8a41d9c9d2217262b742770830202be9c11
MaximilianGaedig <38767445+MaximilianGaedig@users.noreply.github.com>
2021-09-20 22:03:59 +0200

    Fixes for ota updates

--- generate_metadata.py
+++ generate_metadata.py
@@ -11,9 +11,10 @@ zip_path = parser.parse_args().zip
 
 with ZipFile(zip_path) as f:
     with f.open("META-INF/com/android/metadata") as metadata:
-        data = dict(line[:-1].decode().split("=") for line in metadata)
+        data = dict(line[:-1].decode().split("=") for line in metadata)        
         for channel in ("beta", "stable", "testing"):
             with open(path.join(path.dirname(zip_path), data["pre-device"] + "-" + channel), "w") as output:
                 build_id = data["post-build"].split("/")[3]
+                # fix fingerprint spoofing
                 incremental = data["post-build"].split("/")[4].split(":")[0]
-                print(incremental, data["post-timestamp"], build_id, channel, file=output)
+                print(data["post-build-incremental"], data["post-timestamp"], build_id, channel,file=output)
